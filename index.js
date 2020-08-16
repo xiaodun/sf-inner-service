8f92bdecc62137b95dbfc8b087d4afd3dbeb5007
Xiao dun <1947147481@qq.com>
2020-08-16 16:39:14 +0800

    Revert "支持delete"
    This reverts commit e3e0fc04a774f60025a0e310aa7512e084593795.

--- index.js
+++ index.js
@@ -4,19 +4,19 @@
  支持get和post方式
 */
 exports.start = function (config) {
-  var http_os = require('http');
-  var file_os = require('fs');
-  var url_os = require('url');
-  var IPv4 = 'localhost';
-  var os = require('os');
-  var formidable_os = require('formidable');
-  var WebSocketServer = require('ws').Server;
+  var http_os = require("http");
+  var file_os = require("fs");
+  var url_os = require("url");
+  var IPv4 = "localhost";
+  var os = require("os");
+  var formidable_os = require("formidable");
+  var WebSocketServer = require('ws').Server
   //动态的获取本机IP地址
   let network = os.networkInterfaces();
   for (let key in network) {
     let env = network[key];
     for (var i = 0; i < env.length; i++) {
-      if (env[i].family == 'IPv4' && env[i].address != '127.0.0.1') {
+      if (env[i].family == "IPv4" && env[i].address != "127.0.0.1") {
         IPv4 = env[i].address;
       }
     }
@@ -32,112 +32,102 @@ exports.start = function (config) {
       console.log(`webSocket is running ${config.webSocket.port}`);
       try {
         //读取websocket.js 没有则创建
-        let websocketPath = config.dataFloderName + '/websocket.js';
+        let websocketPath = config.dataFloderName + "/websocket.js"
         if (!file_os.existsSync(websocketPath)) {
-          file_os.writeFileSync(
-            websocketPath,
-            `(function(){
+          file_os.writeFileSync(websocketPath, `(function(){
         return function(argData){
             let senData = {}
             return senData;
         }
-    })()`,
-          );
+    })()`);
         }
 
         wss.on('connection', function (ws) {
+
+
           ws.on('message', function (message) {
-            let websocketjs = file_os.readFileSync(
-              websocketPath,
-              'utf-8',
-            );
+            let websocketjs = file_os.readFileSync(websocketPath, "utf-8");
             // 广播消息
             let result = eval(websocketjs)(JSON.parse(message));
 
             ws.send(JSON.stringify(result));
-          });
+          })
         });
+
       } catch (error) {
         console.log(error);
       }
+
     } catch (error) {
       throw new Error(error);
     }
-  }
+
+
+  };
   var server = http_os.createServer(function (request, response) {
     try {
-      var urlElementsArr = request.url
-        .slice(1, request.url.length)
-        .split('/');
+      var urlElementsArr = request.url.slice(1, request.url.length).split("/");
       console.log(`${IPv4}:${config.port}${request.url}`);
       let prefix = urlElementsArr[0],
         appName = urlElementsArr[1],
-        moduleNames = urlElementsArr.slice(
-          2,
-          urlElementsArr.length - 2,
-        ),
+        moduleNames = urlElementsArr.slice(2, urlElementsArr.length - 2),
         dataName = urlElementsArr[urlElementsArr.length - 2],
         command;
-      let paramsPos = urlElementsArr[
-        urlElementsArr.length - 1
-      ].indexOf('?');
+      let paramsPos = urlElementsArr[urlElementsArr.length - 1].indexOf("?");
       if (paramsPos == -1) {
         command = urlElementsArr[urlElementsArr.length - 1];
       } else {
-        command = urlElementsArr[urlElementsArr.length - 1].slice(
-          0,
-          paramsPos,
-        );
+        command = urlElementsArr[urlElementsArr.length - 1].slice(0, paramsPos);
       }
+
 
       //视频播放
       let external = {};
 
       let floderPathArr = (config.abspath
-        ? config.abspath.split('/')
+        ? config.abspath.split("/")
         : []
       ).concat([
         config.dataFloderName,
         prefix,
         appName,
         ...moduleNames,
-        dataName,
+        dataName
       ]);
       var rootFloder = {
         path:
-          (config.abspath ? config.abspath + '/' : '') +
+          (config.abspath ? config.abspath + "/" : "") +
           [
             config.dataFloderName,
             prefix,
             appName,
             ...moduleNames,
-            dataName,
-          ].join('/'),
+            dataName
+          ].join("/")
       };
 
-      let countPath = '';
-      floderPathArr.forEach((el) => {
-        countPath += el + '/';
+      let countPath = "";
+      floderPathArr.forEach(el => {
+        countPath += el + "/";
         if (!file_os.existsSync(countPath)) {
           file_os.mkdirSync(countPath);
         }
       });
       //创建文件
-      rootFloder.dataPath =
-        rootFloder.path + '/' + dataName + '.json';
+      rootFloder.dataPath = rootFloder.path + "/" + dataName + ".json";
 
       if (!file_os.existsSync(rootFloder.dataPath)) {
-        file_os.writeFileSync(rootFloder.dataPath, '');
+        file_os.writeFileSync(rootFloder.dataPath, "");
       }
 
       //生命周期文件
-      rootFloder.lifeCyclePath = rootFloder.path + '/lifeCycle.js';
+      rootFloder.lifeCyclePath = rootFloder.path + "/lifeCycle.js";
 
       let lifeCycleModule = {};
       if (file_os.existsSync(rootFloder.lifeCyclePath)) {
         //如果存在生命周期函数
         lifeCycleModule = eval(
-          file_os.readFileSync(rootFloder.lifeCyclePath, 'utf-8'),
+          file_os.readFileSync(rootFloder.lifeCyclePath, "utf-8")
         )();
       }
 
@@ -168,7 +158,7 @@ exports.start = function (config) {
         lifeCycleModule.dealCommand(command, external);
 
       if (commandResults) {
-        if (commandResults.type === 'video') {
+        if (commandResults.type === "video") {
           //对视频文件的统一处理
           let { filePath } = commandResults;
           file_os.stat(filePath, function (error, stats) {
@@ -180,26 +170,23 @@ exports.start = function (config) {
               // 416 Wrong range
               return response.sendStatus(416);
             }
-            var positions = range.replace(/bytes=/, '').split('-');
+            var positions = range.replace(/bytes=/, "").split("-");
             var start = parseInt(positions[0], 10);
             var total = stats.size;
-            var end = positions[1]
-              ? parseInt(positions[1], 10)
-              : total - 1;
+            var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
             var chunksize = end - start + 1;
             response.writeHead(206, {
-              'Content-Range':
-                'bytes ' + start + '-' + end + '/' + total,
-              'Accept-Ranges': 'bytes',
-              'Content-Length': chunksize,
+              "Content-Range": "bytes " + start + "-" + end + "/" + total,
+              "Accept-Ranges": "bytes",
+              "Content-Length": chunksize
             });
 
             var stream = file_os
               .createReadStream(filePath, { start: start, end: end })
-              .on('open', function () {
+              .on("open", function () {
                 stream.pipe(response);
               })
-              .on('error', function (err) {
+              .on("error", function (err) {
                 response.end(err);
               });
           });
@@ -209,76 +196,69 @@ exports.start = function (config) {
       }
 
       //创建命令文件  此处代码需要在dealCommand生命周期钩子后执行，防止在处理静态资源的时候额外的创建.js
-      rootFloder.commandPath =
-        rootFloder.path + '/' + command + '.js';
+      rootFloder.commandPath = rootFloder.path + "/" + command + ".js";
       if (!file_os.existsSync(rootFloder.commandPath)) {
-        file_os.writeFileSync(
-          rootFloder.commandPath,
-          commandTemplate,
-        );
+        file_os.writeFileSync(rootFloder.commandPath, commandTemplate);
       }
       //解析参数
 
-      if (request.method.toUpperCase() == 'POST') {
+      if (request.method.toUpperCase() == "POST") {
         /**
          * 文件上传
          */
 
         if (
-          ~(request.headers['content-type'] || '').indexOf(
-            'multipart/form-data',
+          ~(request.headers["content-type"] || "").indexOf(
+            "multipart/form-data"
           )
         ) {
           var postData = {
-            files: [],
+            files: []
           };
           var form = new formidable_os.IncomingForm();
           form.maxFileSize = 5 * 1024 * 1024 * 1024;
           if (lifeCycleModule.getUploadPath) {
             form.uploadDir = lifeCycleModule.getUploadPath(external);
           } else {
-            form.uploadDir = process.cwd() + '/' + rootFloder.path;
+            form.uploadDir = process.cwd() + "/" + rootFloder.path;
           }
           form.parse(request, function (error, fileds, files) {
             if (error) {
               // 超过指定大小时的报错
             }
           });
-          form.on('file', function (name, file) {
+          form.on("file", function (name, file) {
             //写入文件名和路径
             postData.files.push({
               name: file.name,
               type: file.type,
-              flag: file.path.substr(file.path.lastIndexOf('\\') + 1),
+              flag: file.path.substr(file.path.lastIndexOf("\\") + 1)
             });
           });
-          form.on('end', function () {
+          form.on("end", function () {
             executeCommand(postData);
           });
         } else {
           /**
            * 数据读取完毕就会执行的监听方法
            */
-          var postData = '';
+          var postData = "";
 
-          request.addListener('data', function (data) {
+          request.addListener("data", function (data) {
             postData += data;
           });
-          request.addListener('end', function () {
+          request.addListener("end", function () {
             executeCommand(JSON.parse(postData || null));
           });
         }
-      } else if (
-        request.method.toUpperCase() == 'GET' ||
-        request.method.toUpperCase() == 'DELETE'
-      ) {
+      } else if (request.method.toUpperCase() == "GET") {
         var params = url_os.parse(request.url, true).query;
         executeCommand(params);
       }
       function createFloder(list) {
         try {
-          let path = '';
-          list.forEach((el) => {
+          let path = "";
+          list.forEach(el => {
             path += el;
             if (!file_os.existsSync(path)) {
               file_os.mkdirSync(path);
@@ -293,23 +273,19 @@ exports.start = function (config) {
           //执行命令
           //获取json数据
           var data = JSON.parse(
-            file_os.readFileSync(rootFloder.dataPath, 'utf-8') ||
-              null,
+            file_os.readFileSync(rootFloder.dataPath, "utf-8") || null
           );
           var cloneData = JSON.parse(JSON.stringify(data));
           // var result = eval(new String(file_os.readFileSync(rootFloder.commandPath)))(cloneData,params);
           var result = eval(
-            file_os.readFileSync(rootFloder.commandPath, 'utf-8'),
+            file_os.readFileSync(rootFloder.commandPath, "utf-8")
           )(cloneData, params, external);
           if (result.isDelete) {
             let path;
             if (lifeCycleModule.getDeleteFilePath) {
-              path = lifeCycleModule.getDeleteFilePath(
-                external,
-                result,
-              );
+              path = lifeCycleModule.getDeleteFilePath(external, result);
             } else {
-              path = rootFloder.path + '/' + result.file.flag;
+              path = rootFloder.path + "/" + result.file.flag;
             }
             file_os.unlinkSync(path);
           }
@@ -319,17 +295,16 @@ exports.start = function (config) {
               //防止数据遭到意外覆盖  比如忘记返回数据！
               file_os.writeFileSync(
                 rootFloder.dataPath,
-                JSON.stringify(result.data, null, 4),
+                JSON.stringify(result.data, null, 4)
               );
             } else {
               response.writeHead(500, {
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json"
               });
               response.end(
                 JSON.stringify({
-                  message:
-                    '重写数据时发生错误,没有得到有效的返回数据',
-                }),
+                  message: "重写数据时发生错误,没有得到有效的返回数据"
+                })
               );
               return;
             }
@@ -340,30 +315,26 @@ exports.start = function (config) {
             let path;
 
             if (lifeCycleModule.getDownloadFilePath) {
-              path = lifeCycleModule.getDownloadFilePath(
-                external,
-                result,
-              );
+              path = lifeCycleModule.getDownloadFilePath(external, result);
             } else {
-              path = rootFloder.path + '/' + result.file.flag;
+              path = rootFloder.path + "/" + result.file.flag;
             }
             let readStream = file_os.ReadStream(path);
             response.writeHead(200, {
-              'Content-Type': 'application/octet-stream',
-              'Accept-Ranges': 'bytes',
+              "Content-Type": "application/octet-stream",
+              "Accept-Ranges": "bytes"
             });
-            readStream.on('close', function () {
+            readStream.on("close", function () {
               response.end();
             });
             readStream.pipe(response);
           } else {
             //返回结果
             response.writeHead(result.response.code, {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Methods':
-                'DELETE,PUT,POST,GET,OPTIONS',
-              'Access-Control-Allow-Origin': '*',
-              'Access-Control-Allow-Headers': 'content-type',
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Methods": "DELETE,PUT,POST,GET,OPTIONS",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "content-type"
             });
             response.end(JSON.stringify(result.response.data));
           }
@@ -384,12 +355,10 @@ exports.start = function (config) {
   server.listen(config.port, function () {
     console.log(`service is running ${config.port}`);
   });
-  server.on('error', function (error) {
+  server.on("error", function (error) {
     console.log(error);
     if (error.toString().indexOf(`listen EADDRINUSE`) !== -1) {
-      console.log(
-        `${config.port}端口被占用,可能是当前应用,也可能是其他应用`,
-      );
+      console.log(`${config.port}端口被占用,可能是当前应用,也可能是其他应用`);
     }
   });
 };
